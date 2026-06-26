98e2d7847314caca3d148c1999fcbf590c4cb9fb
wingless-bird <Siddique.mansoor164@gmail.com>
2026-06-26 20:44:03 +0500

    Add vercel routing

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Clean URL format with no extra folders or trailing slashes
-const SUPABASE_URL = 'https://yzvaeyyrzikgiotkfztm.supabase.co'; 
-
-// Use your long PUBLISHABLE / ANON PUBLIC key here (the one starting with eyJ)
-const SUPABASE_ANON_KEY = 'sb_publishable_6YMeCDJsLNdRc5-2f39HMg_55B53064';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
