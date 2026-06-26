4d31291c41af95785b02e7ce7c6436719d7186ad
wingless-bird <Siddique.mansoor164@gmail.com>
2026-06-26 21:52:02 +0500

    debug env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://yzvaeyyrzikgiotkfztm.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_6YMeCDJsLNdRc5-2f39HMg_55B53064';
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
