7360ea4485231583be04ccc2c1868658d0e361a7
wingless-bird <Siddique.mansoor164@gmail.com>
2026-06-26 21:32:39 +0500

    update code changes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://yzvaeyyrzikgiotkfztm.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_FULL_KEY';
+const SUPABASE_ANON_KEY = 'sb_publishable_6YMeCDJsLNdRc5-2f39HMg_55B53064';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
